4928c7ab062e597cea6bff8f1c924f9d5e145eec
Milosz Muszynski <milosz.muszynski@gmail.com>
2026-08-11 13:28:24 +0200

    refactored tests and created lib

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-mod app;
-mod error;
+pub mod app;
 pub mod storage;
-mod wasm;
 
+use wasm_kv_db::{AppError, error, wasm};
 use crate::app::AppState;
 use crate::app::handlers::{delete_value, get_value, list_keys, put_value};
-use crate::error::AppError;
 use axum::{
     Router,
     http::StatusCode,
@@ -81,32 +79,5 @@ async fn shutdown_signal() {
     tokio::select! {
         _ = ctrl_c => {},
         _ = terminate => {},
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::error::AppError;
-    use crate::wasm::WasmGuest;
-
-    #[test]
-    pub fn test_wasm_guest() -> Result<(), AppError> {
-        let wasm_bytes = std::fs::read(
-            "wasm-guests/simple-guest/target/wasm32-unknown-unknown/debug/simple_guest.wasm",
-        )?;
-
-        let mut guest = WasmGuest::new(&wasm_bytes)?;
-
-        // let input = serde_json::from_str(r#"{"key": "value"}"#)?;
-        let input = serde_json::from_str(
-            r#"{"department": "Engineering", "name": "Alice", "personal_email": "alice@gmail.com", "salary": 95000}"#,
-        )?;
-
-        let output = guest.transform_json(&input)?;
-
-        println!("Input:  {}", input.to_string());
-        println!("Output: {}", output.to_string());
-
-        Ok(())
     }
 }

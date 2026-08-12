3c1e7ea0d660c756739e0232a33bc6bf26244c5a
Milosz Muszynski <milosz.muszynski@gmail.com>
2026-08-12 14:45:21 +0200

    passing input key to the guest

--- tests/guest_basic_test.rs
+++ tests/guest_basic_test.rs
@@ -8,15 +8,14 @@ pub fn test_wasm_guest() -> Result<(), AppError> {
     let storage = Storage::new();
     let mut guest = load_guest(storage.clone());
 
-    let input = b"t01:k1".to_vec();
+    let input_key = b"t01:k1".to_vec();
 
     storage.put("t01:k2", b"abbacafe".to_vec())?;
-    // execute will read value from k2 and write it under k1
-    // input will be ignored
-    let output = guest.execute(&input)?;
+    // execute will read value from k2 and write it under input_key
+    let output = guest.execute(&input_key)?;
     let stored = storage.get("t01:k1")?;
 
-    println!("Input:  {}", String::from_utf8_lossy(&input));
+    println!("Input:  {}", String::from_utf8_lossy(&input_key));
     println!("Output: {:?}", hex::encode(output));
     println!("Stored:  {}", String::from_utf8_lossy(&stored));
 
--- wasm-guests/simple-guest/src/lib.rs
+++ wasm-guests/simple-guest/src/lib.rs
@@ -14,19 +14,23 @@ unsafe extern "C" {
 pub extern "C" fn execute(input_len: usize) -> i32 {
     let buf_ptr = &raw mut ARG_BUF as *mut u8;
 
-    let _input = unsafe { std::slice::from_raw_parts(buf_ptr, input_len) };
+    let input_key = unsafe { std::slice::from_raw_parts(buf_ptr, input_len) }.to_vec();
 
-    let k1_key = b"t01:k1".to_vec();
     let k2_key = b"t01:k2".to_vec();
 
     // read value from "t01:k2" and store it under "t01:k1"
-    let k2_value_len = unsafe {
-        host_get(k2_key.as_ptr(), k2_key.len(), buf_ptr, ARG_BUF_SIZE)
-    };
+    let k2_value_len = unsafe { host_get(k2_key.as_ptr(), k2_key.len(), buf_ptr, ARG_BUF_SIZE) };
     assert!(k2_value_len > 0);
 
     // we have ARG_BUF filled out with our k2 value
-    let result = unsafe { host_put(k1_key.as_ptr(), k1_key.len(), buf_ptr, k2_value_len as usize) };
+    let result = unsafe {
+        host_put(
+            input_key.as_ptr(),
+            input_key.len(),
+            buf_ptr,
+            k2_value_len as usize,
+        )
+    };
 
     let output_bytes = result.to_le_bytes();
     unsafe {

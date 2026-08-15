307d369b3ca277f3a18cf4b4efb0b7904acb2b73
Milosz Muszynski <milosz.muszynski@gmail.com>
2026-08-15 13:39:59 +0200

    enriched peristence test

--- tests/persistence_test.rs
+++ tests/persistence_test.rs
@@ -8,6 +8,7 @@ pub async fn test_persistence() -> Result<(), AppError> {
     let storage = Storage::new_with_persistence(&db_path)?;
     storage.put("key1", b"value1".to_vec())?;
     storage.put("key2", b"value2".to_vec())?;
+    storage.append_to_list("key2", b"34".to_vec())?;
     storage.sync()?;
     drop(storage);
 
@@ -15,7 +16,7 @@ pub async fn test_persistence() -> Result<(), AppError> {
 
     let storage = Storage::new_with_persistence(&db_path)?;
     assert_eq!(storage.get("key1")?, b"value1");
-    assert_eq!(storage.get("key2")?, b"value2");
+    assert_eq!(storage.get("key2")?, b"value234");
 
     Ok(())
 }
